b58501f470cf7818c5c694e782f928396ed2d459
Michael Bausano <michael@joinpouch.com>
2018-12-01 17:14:41 +0000

    xd

--- index.js
+++ index.js
@@ -13,16 +13,40 @@ exports.handler = (event, _, callback) => {
         }
       }).promise()
     })
-    .then((result) => {
-      console.log('res', result)
+    .then(({ Items }) => {
+      if (!Items || !Items.length) {
+        throw { status: 404, message: 'CodeNotFound' }
+      }
+
+      const code = Items.filter(item => item.used.BOOL === false).pop()
+
+      if (!code) {
+        throw { status: 406, message: 'CodeAlreadyRedeem' }
+      }
+
+      return code.id.S
     })
+    .then((id) => {
+      return ddb.updateItem({
+        TableName: process.env.DDB_TABLE,
+        Key: { id: { S: id } },
+        UpdateExpression: 'SET #used = :b',
+        ExpressionAttributeNames: {
+          '#used': 'used'
+         },
+         ExpressionAttributeValues: {
+          ':b': { BOOL: true }
+         },
+      }).promise()
+    })
+    .then(() => ({ status: 200, body: '' }))
     .catch((error = {}) => {
       console.log(error)
       return {
-        status: error.status || 404,
+        status: error.status || 500,
         body: {
-          status: error.status || 404,
-          message: error.message || 'CodeNotFound'
+          status: error.status || 500,
+          message: error.message || 'InternalServerError'
         }
       }
     })
